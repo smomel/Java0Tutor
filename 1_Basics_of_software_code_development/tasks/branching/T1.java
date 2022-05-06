73e9c6bcc512ee0358785758402ba447a65bf895
Serg Omelyanchik <smomel@gmail.com>
2022-05-06 20:37:59 +0300

    ветвления

---
+++ 1_Basics_of_software_code_development/tasks/branching/T1.java
@@ -0,0 +1,18 @@
+/*
+ * Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли 
+ * он прямоугольным.
+ */
+
+import java.util.Scanner;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		int angleA, angleB;
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
