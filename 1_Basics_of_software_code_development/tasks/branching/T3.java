a7abfb24843a290805c5573a53e669f2c5a6e550
smomel <smomel@gmail.com>
2022-05-07 18:08:40 +0300

    commit T3.java

--- 1_Basics_of_software_code_development/tasks/branching/T3.java
+++ 1_Basics_of_software_code_development/tasks/branching/T3.java
@@ -5,26 +5,49 @@
 public class T3 {
 
 	public static void main(String[] args) {
-		int x1 = 8, y1 = -1;
-		int x2 = -3, y2 = -3;
-		int x3 = 13, y3 = 0;
-		int max;
-		boolean res = false;
+		// задаем координаты точек
+		double x1 = -4, y1 = 4;
+		double x2 = -1, y2 = -1;
+		double x3 = 2, y3 = -6;
+		
+		double max, katet1, katet2;
+		double angleA, angleB;
+		boolean res;
 		
 		// 1. Точки будут на одной прямой, если совпадают все координаты по х или по у
 		if ( ( (x1 == x2) && (x1 == x3) ) || ( (y1 == y2) && (y1 == y3) ) ) {
 			res = true;
 		} else {
 			
-			// 2. Точки будут на одной прямой, если они лежат на гипотенузе прямоугольного треутольника
+			// 2. Точки будут на одной прямой, если они лежат на гипотенузе прямоугольного треугольника
+			// для этого вычислим последовательно катеты треугольника и значения углов, если углы равны, то точки на гипотенузе
 			
 			// упорядочим точки на координатной плоскости в направлении по оси Х слева направо
 			if (x1 > x2) { max = x1; x1 = x2; x2 = max;	max = y1; y1 = y2; y2 = max;}			
 			if (x1 > x3) { max = x1; x1 = x3; x3 = max;	max = y1; y1 = y3; y3 = max;}			
-			if (x2 > x3) { max = x2; x2 = x3; x3 = max; max = y2; y2 = y3; y3 = max;}			
+			if (x2 > x3) { max = x2; x2 = x3; x3 = max; max = y2; y2 = y3; y3 = max;}	
+			
+			// определяем значение катетов первого треугольника (х1, у1) - (х2, у2)
+			katet1 = Math.abs(x2 - x1);
+			katet2 = Math.abs(y2 - y1);
+			
+			// находим значение угла первого треугольника
+			angleA = Math.atan( (double) katet1 / katet2 );
+			
+			// определяем значение катетов второго треугольника (х1, у1) - (х3, у3)
+			katet1 = Math.abs(x3 - x1);
+			katet2 = Math.abs(y3 - y1);
+			
+			// находим значение угла первого треугольника
+			angleB = Math.atan( (double) katet1 / katet2 );
+			
+			// сравниваем углы
+			res = (angleA == angleB) ? true : false;			
 						
-			System.out.println("x = " + x1 + ", " + x2 + ", " + x3);
-			System.out.println("y = " + y1 + ", " + y2 + ", " + y3);
+			//System.out.println("x = " + x1 + ", " + x2 + ", " + x3);
+			//System.out.println("y = " + y1 + ", " + y2 + ", " + y3);
+			//System.out.println("катеты = " + katet1 + ":" + katet2);
+			//System.out.println("Угол А = " + angleA + ", угол В = " + angleB);
 		}		
 		
 		if (res) {
