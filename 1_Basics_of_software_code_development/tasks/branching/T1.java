4006d7dd74d1dede6a4f20c3851eb68b88382326
smomel <smomel@gmail.com>
2022-05-07 14:36:53 +0300

    update T2.java

--- 1_Basics_of_software_code_development/tasks/branching/T1.java
+++ 1_Basics_of_software_code_development/tasks/branching/T1.java
@@ -11,6 +11,14 @@ public class T1 {
 		int angleA, angleB;
 		Scanner input = new Scanner(System.in);
 		
+		// запрашиваем у пользователя ввод начения угла А
+		System.out.print("Введите значение угла 'A': ");
+		angleA = input.nextInt();
+		
+		// запрашиваем у пользователя ввод значения угла В
+		System.out.print("Введите значение угла 'B': ");
+		angleB = input.nextInt();
+		
 				
 	}
 
