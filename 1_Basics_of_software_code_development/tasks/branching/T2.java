d2ac243d74cabf396c01d7b218699f65e0700962
smomel <smomel@gmail.com>
2022-05-07 15:12:25 +0300

    решение задачи 2

--- 1_Basics_of_software_code_development/tasks/branching/T2.java
+++ 1_Basics_of_software_code_development/tasks/branching/T2.java
@@ -1,9 +1,22 @@
+/*
+ * Найти max{min(a, b), min(c, d)}.
+ */
 
 public class T2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double a = 3;
+		double b = 5;
+		double c = 10;
+		double d = 20;
+		double min1, min2, max;
+		
+		min1 = (a < b) ? a : b;
+		min2 = (c < d) ? c : d;
+		
+		max = (min1 > min2) ? min1 : min2;
+		
+		System.out.println("Результат: " + max);		
 	}
 
 }
