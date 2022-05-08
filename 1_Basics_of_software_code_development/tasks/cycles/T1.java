32d455df074d32fddf31e709ee005e3806a93504
smomel <smomel@gmail.com>
2022-05-08 19:32:13 +0300

    commit

--- 1_Basics_of_software_code_development/tasks/cycles/T1.java
+++ 1_Basics_of_software_code_development/tasks/cycles/T1.java
@@ -3,23 +3,42 @@
  * все числа от 1 до введенного пользователем числа.
  */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class T1 {
 
 	public static void main(String[] args) {
-		long n, i, sum = 0;
+		long n, sum = 0;
+		boolean ok = true;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Введите целое положительное число: ");
-		n = input.nextLong();
-		
-		for (i = 1; i <= n; i++) {
-			sum += i;
+		try {
+			System.out.print("Введите целое положительное число: ");
+			n = input.nextLong();
+			
+			for (long i = 1; i <= n; i++) {								
+				sum += i;
+				
+				if (sum < 0) {
+					System.out.println("Переполнение. i = " + i);
+					ok = false;
+					break;
+				}				
+			}
+			
+			if (ok) {
+				System.out.println("Сумма чисел от 1 до " + n + " равна: " + sum);
+			}			
+						
+		} catch (InputMismatchException e) {
+			if (e.getMessage() == null) {
+				System.out.println("Неверный формат ввода.");
+			} else {
+				System.out.println("Максимальное значение 'n' может быть: " + Long.MAX_VALUE);
+			}
 		}
-		
-		System.out.println("Сумма чисел от 1 до " + n + " равна: " + sum);
-		
+				
         input.close();
 	}
 
