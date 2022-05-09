4502c57a75a620a32a66dc7f341077e48e75a053
smomel <smomel@gmail.com>
2022-05-09 21:12:18 +0300

    change

--- 2_Algorithmization/tasks/one_dimensional_arrays/T1.java
+++ 2_Algorithmization/tasks/one_dimensional_arrays/T1.java
@@ -5,8 +5,9 @@
 public class T1 {
 
 	public static void main(String[] args) {
-		int[] an; 
-		int K = 2, sum = 0, n = 20;
+		int[] an;
+		int n = 20;
+		int K = 2, sum = 0;
 		
 		an = new int[n];
 		System.out.println("Элементы массива:");
@@ -14,8 +15,8 @@ public class T1 {
 		// заполним массив случайными натуральными числами
 		for (int i = 0; i < n; i++) {
 			an[i] = (int) (Math.random() * n + 1);
-			
 			System.out.print(an[i] + " ");
+			
 			// если число кратно К, то суммируем его
 			if (an[i] % K == 0) sum += an[i];
 		}
