d8341212e8235e5aebf1e7302c24d81b62c1f4b6
smomel <smomel@gmail.com>
2022-05-09 19:49:09 +0300

    commit

---
+++ 2_Algorithmization/tasks/one_dimensional_arrays/T1.java
@@ -0,0 +1,27 @@
+/*
+ * В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+ */
+
+public class T1 {
+
+	public static void main(String[] args) {
+		int[] an; 
+		int K = 2, sum = 0, n = 20;
+		
+		an = new int[n];
+		System.out.println("Элементы массива:");
+		
+		// заполним массив случайными натуральными числами до 100
+		for (int i = 0; i < n; i++) {
+			an[i] = (int) (Math.random() * 100 + 1);
+			
+			System.out.print(an[i] + " ");
+			// если число кратно К, то суммируем его
+			if (an[i] % K == 0) sum += an[i];
+		}
+		
+		System.out.println();
+		System.out.println("Сумма элементов, кратных " + K + " = " + sum);
+	}
+
+}
