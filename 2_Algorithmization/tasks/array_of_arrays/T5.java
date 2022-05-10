0792995feac2d04c7be022cea9ee7253e20bb3fe
smomel <smomel@gmail.com>
2022-05-10 19:40:15 +0300

    commit

---
+++ 2_Algorithmization/tasks/array_of_arrays/T5.java
@@ -0,0 +1,29 @@
+/*
+ * Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
+ *
+ */
+public class T5 {
+
+	public static void main(String[] args) {
+		int n = 6;
+		int[][] arr = new int[n][n];
+				
+		System.out.println("Полученная матрица: ");
+		
+		// формируем матрицу и выводим на экран		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i; j ++) {				
+				arr[i][j] = i + 1;				
+			}
+		}
+		
+		// выведем результат на экран
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
